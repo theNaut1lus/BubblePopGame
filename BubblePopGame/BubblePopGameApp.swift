58bc36fb3425c9c99953ba4a3ef6050700e0ad8d
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-02 15:24:02 +1100

    Initial Commit

---
+++ BubblePopGame/BubblePopGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopGameApp.swift
+//  BubblePopGame
+//
+//  Created by Sidak Singh Aulakh on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
