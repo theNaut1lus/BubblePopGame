9bb5731a78bc0115ae801d3f082bee36ca88e2cf
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-10 18:33:14 +1000

    eliminate double nav view
    double nav view in both parent and settings view causing double back button issues

--- BubblePopGame/SettingsView.swift
+++ BubblePopGame/SettingsView.swift
@@ -14,47 +14,45 @@ struct SettingsView: View {
     @EnvironmentObject var highScoreViewModel: HighScoreViewModel
     
     var body: some View {
-        NavigationView {
-            ZStack{
-                Rectangle()
-                    .fill(.titlePageBackground)
-                    .ignoresSafeArea()
-                VStack{
-                    Label("SettingsView", systemImage: "")
-                        .foregroundStyle(.regularMaterial)
-                        .fontWeight(.black)
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    Text("Enter Name")
-                        .foregroundStyle(.regularMaterial)
-                        .fontWeight(.black)
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    TextField("Enter Name", text: $startGameViewModel.name)
-                        .padding(.all, 20.0)
-                        .textFieldStyle(.roundedBorder)
-                    Spacer()
-                    Text("Game Time")
-                        .foregroundStyle(.regularMaterial)
-                        .fontWeight(.bold)
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    Slider(value: $startGameViewModel.gameTime, in: 0...60, step: 1)
-                        .padding(.horizontal, 20.0)
-                    
-                    Text("\(Int(startGameViewModel.gameTime))")
-                        .padding()
-                    Text("Max number of bubbles")
-                        .foregroundStyle(.regularMaterial)
-                        .fontWeight(.bold)
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    Slider(value: $startGameViewModel.numOfBubbles, in: 0...15, step: 1)
-                        .padding(.horizontal, 20.0)
-                    Text("\(Int(startGameViewModel.numOfBubbles))")
-                        .padding()
-                    NavigationLink(destination: StartGameView().environmentObject(startGameViewModel), label: {Text("Start Game!").font(.title)})
-                        
-                }
+        ZStack{
+            Rectangle()
+                .fill(.titlePageBackground)
+                .ignoresSafeArea()
+            VStack{
+                Label("SettingsView", systemImage: "")
+                    .foregroundStyle(.regularMaterial)
+                    .fontWeight(.black)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text("Enter Name")
+                    .foregroundStyle(.regularMaterial)
+                    .fontWeight(.black)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                TextField("Enter Name", text: $startGameViewModel.name)
+                    .padding(.all, 20.0)
+                    .textFieldStyle(.roundedBorder)
+                Spacer()
+                Text("Game Time")
+                    .foregroundStyle(.regularMaterial)
+                    .fontWeight(.bold)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Slider(value: $startGameViewModel.gameTime, in: 0...60, step: 1)
+                    .padding(.horizontal, 20.0)
+                
+                Text("\(Int(startGameViewModel.gameTime))")
+                    .padding()
+                Text("Max number of bubbles")
+                    .foregroundStyle(.regularMaterial)
+                    .fontWeight(.bold)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Slider(value: $startGameViewModel.numOfBubbles, in: 0...15, step: 1)
+                    .padding(.horizontal, 20.0)
+                Text("\(Int(startGameViewModel.numOfBubbles))")
+                    .padding()
+                NavigationLink(destination: StartGameView().environmentObject(startGameViewModel), label: {Text("Start Game!").font(.title)})
                 
             }
+            
         }
     }
 }
